ca9df3a252a9a6b272a9bd9f0c8f6f6c5a1a00bf
Jose Plasencia <josejpd3@gmail.com>
2022-11-10 16:23:57 -0500

    home route findAll and render all-posts handlebar in controllers/home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+          include: [
+            {
+              model: User
+            },
+          ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts', { 
+          posts, 
+          logged_in: req.session.logged_in 
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
