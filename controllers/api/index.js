5c0905c0a084c3d216b0c2258cf60a5418565912
Jose Plasencia <josejpd3@gmail.com>
2022-11-09 23:37:45 -0500

    set router paths in controllers/api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,3 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
