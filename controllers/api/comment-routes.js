ba3decc39ccd68eded08725c0bcd213c3279a32a
Jose Plasencia <josejpd3@gmail.com>
2022-11-11 21:36:29 -0500

    create new Comment in controllers/api/comment-routes.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,7 +4,11 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-
+    const newComment = await Comment.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+    res.json(newComment);
   } catch (err) {
     res.status(500).json(err);
   }
