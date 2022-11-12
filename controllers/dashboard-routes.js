5a5c0868a339e9fc6a0fd225691b08a12f04d46f
Jose Plasencia <josejpd3@gmail.com>
2022-11-11 20:07:36 -0500

    Post.findAll, render 'dashboard', and err handler in controllers/dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,8 +5,22 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User
+                },
+            ],
+            where: {
+                user_id: req.session.user_id
+            }
+        });
+        const posts = postData.map((post) => post.get({ plain: true}));
+        res.render('dashboard', {
+            posts,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
-
+        res.status(500).json(err);
     }
-});
+})
