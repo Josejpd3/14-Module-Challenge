a9ffcd61aae0f953a7d52f2b2a6413278a18a7a5
Jose Plasencia <josejpd3@gmail.com>
2022-11-11 21:21:52 -0500

    try/catch in controllers/api/user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
+    try {
 
+    } catch (err) {
+      res.status(400).json(err);
+    }
   });
