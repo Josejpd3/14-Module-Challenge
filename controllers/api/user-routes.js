9c8e71125892986520b0a89ee703add672e369d6
Jose Plasencia <josejpd3@gmail.com>
2022-11-12 02:27:10 -0500

    log in and save session in controllers/api/user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -30,6 +30,12 @@ router.post('/', async (req, res) => {
         res.json({ message: 'Incorrect password, please try again' });
         return;
       }
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        res.status(200)
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
     } catch (err) {
       res.status(400).json(err);
     }
