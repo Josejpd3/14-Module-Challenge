71e6db30e8334677584e2a1558ca4804449cdcee
Jose Plasencia <josejpd3@gmail.com>
2022-11-11 20:03:35 -0500

    required express, sequelize, ../models and ..utils/auth.js in controllers/dashboard-routes.api

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { where } = require('sequelize');
+const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
