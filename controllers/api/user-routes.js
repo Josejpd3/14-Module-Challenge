ede28df84ed0e5b4ac2833ace84c3c3a7cdafc5f
Jose Plasencia <josejpd3@gmail.com>
2022-11-12 02:30:06 -0500

    when logout destroy session in controllers/api/user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,5 +42,11 @@ router.post('/', async (req, res) => {
   });
 
   router.post('/logout', (req, res) => {
-
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
   });
