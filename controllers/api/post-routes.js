969186735c342597b75907a7d5239eb448908804
Jose Plasencia <josejpd3@gmail.com>
2022-11-12 02:38:52 -0500

    router.post '/' withAUth in controllers/api/post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+
+});
