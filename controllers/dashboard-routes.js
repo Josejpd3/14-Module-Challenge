fca1facafa49ef6ec6120070e6a1dd9d714e1722
Jose Plasencia <josejpd3@gmail.com>
2022-11-11 20:05:06 -0500

    '/'router get withAuth in controllers/dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,7 @@ const router = require('express').Router();
 const { where } = require('sequelize');
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+
+});
