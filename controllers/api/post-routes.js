28d1b94ddca356f20b0095232808b04461202ad2
Jose Plasencia <josejpd3@gmail.com>
2022-11-12 02:39:14 -0500

    try/catch in controllers/api/post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,5 +3,9 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-
+  try {
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
