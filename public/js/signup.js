c841a4af06c5be14e0ce3a9b414513aa118bc6bb
Jose Plasencia <josejpd3@gmail.com>
2022-11-13 06:23:48 -0500

    if all inputs are valid POST to '/api/user' in public/js/signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,4 +3,12 @@ const signup = async (e) => {
     const username = document.querySelector('#signupUsername').value.trim();
     const email = document.querySelector('#signupEmail').value.trim();
     const password = document.querySelector('#signupPassword').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
 };
