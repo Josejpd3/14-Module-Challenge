5bdcbad8889b88974fe7e1b064d363614d53b7ce
Jose Plasencia <josejpd3@gmail.com>
2022-11-13 06:27:30 -0500

    getting input values in public/js/login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
 const login = async (e) => {
     e.preventDefault();
+    const email = document.querySelector('#loginEmail').value.trim();
+    const password = document.querySelector('#loginPassword').value.trim();
 };
