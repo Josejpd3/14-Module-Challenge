488bb401ccb0b646cdb7eab94b2833c111e82e86
Jose Plasencia <josejpd3@gmail.com>
2022-11-13 06:22:26 -0500

    getting input values in public/js/signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,3 +1,6 @@
 const signup = async (e) => {
     e.preventDefault();
+    const username = document.querySelector('#signupUsername').value.trim();
+    const email = document.querySelector('#signupEmail').value.trim();
+    const password = document.querySelector('#signupPassword').value.trim();
 };
