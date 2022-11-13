1488fc85fc0b461de519602a42f5a4b80c2d3cae
Jose Plasencia <josejpd3@gmail.com>
2022-11-13 06:29:02 -0500

    if all values are valid POST '/api/user/login' in public/js/login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,4 +2,12 @@ const login = async (e) => {
     e.preventDefault();
     const email = document.querySelector('#loginEmail').value.trim();
     const password = document.querySelector('#loginPassword').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
 };
