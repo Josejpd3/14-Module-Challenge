c6dd33521c694f2047cbfded662771e92d3cb0bf
Jose Plasencia <josejpd3@gmail.com>
2022-11-14 14:37:29 -0500

    try/catch in controllers/new-post-route.js

--- controllers/new-post-route.js
+++ controllers/new-post-route.js
@@ -3,5 +3,9 @@ const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
+    try {
 
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
