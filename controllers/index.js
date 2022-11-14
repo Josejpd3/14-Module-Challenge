2e91f5197264613a02f1b53cd064df03d722c68e
Jose Plasencia <josejpd3@gmail.com>
2022-11-14 15:23:07 -0500

    required /new-post-route and added new route '/post' in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
+const newPost = require('./new-post-route');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/post', newPost);
 router.use('/api', apiRoutes);
 
 module.exports = router;
