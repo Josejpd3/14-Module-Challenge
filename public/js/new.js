726fea076abb669cb598848cf28b0fd5381ea6f0
Jose Plasencia <josejpd3@gmail.com>
2022-11-14 15:53:37 -0500

    if proper input, POST title and content to DB in public/js/new.js

--- public/js/new.js
+++ public/js/new.js
@@ -2,4 +2,13 @@ const post = async function (e) {
     e.preventDefault();
     const title = document.querySelector('#title').value;
     const content = document.querySelector('#text').value;
+
+    if (content && title) {
+        // send inputed data
+        await fetch('/api/post', {
+            method: 'POST',
+            body: JSON.stringify({title, content}),
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
 };
